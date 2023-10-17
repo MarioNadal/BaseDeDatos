0a0271b7c36b3edbf44d660551f6a6056c3ea809
MarioNadal <220056@iessanalberto.com>
2023-10-17 11:20:06 +0200

    17/10/2023

--- src/main/java/code/Conexion.java
+++ src/main/java/code/Conexion.java
@@ -1,2 +1,25 @@
-package code;public class Conexion {
+package code;
+
+import java.lang.module.InvalidModuleDescriptorException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:mariadb//localhost:6606/PERSONAL";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "admin";
+
+    public static Connection concentar(){
+        Connection conexion = null;
+
+        try{
+            conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+        }catch(SQLException ex){
+            System.out.println("Error en la conexión");
+        }catch(InvalidModuleDescriptorException ex){
+            System.out.println("Error PAM");
+        }
+        return conexion;
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class main {
+import libs.Leer;
+
+public class main {
+    public static void main(String[]args){
+        String guiones = "-".repeat(20);
+        int menu;
+        boolean salir = false;
+        do
+
+        {
+            System.out.println(guiones);
+            System.out.println("1.");
+            System.out.println("0.Salir");
+            System.out.println(guiones);
+            menu = Leer.introduceEntero("Introduce el número del menú: ");
+            System.out.println(guiones);
+
+            switch (menu) {
+                case 1 -> ;
+                case 0 -> salir = true;
+            }
+        }while(!salir);
+    }
 }
