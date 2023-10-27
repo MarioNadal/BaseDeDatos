5dab922d7fe67ece0d4665cc03cf56cd8b01a59a
MarioNadal <220056@iessanalberto.com>
2023-10-27 10:26:16 +0200

    Lo antiguo con mysql 27/10/2023

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 
 public class main {
     public static void main(String[]args) {
+        //Con  mySQL
         try(Connection miCon = ConexionBD.conectar("miBD")){
             //Crear la tabla departamentos
             String tablaDep = "CREATE TABLE departamentos (\n" +
